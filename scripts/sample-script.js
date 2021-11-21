1703b80439f0608304cc33329ae6436fb5c46acb
Pradhit Gosula <pradhitg@gmail.com>
2021-11-20 17:43:42 -0800

    small changes

--- scripts/sample-script.js
+++ scripts/sample-script.js
@@ -14,12 +14,9 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const nfuse = await hre.ethers.getContractFactory("nfuse");
 
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+  // do something in here if you want
 }
 
 // We recommend this pattern to be able to use async/await everywhere
